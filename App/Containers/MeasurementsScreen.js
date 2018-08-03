069ab72475d5b1418b8ef3b3292cb68f5a785638
Claire Ryberg <csr6ad@virginia.edu>
2018-08-03 12:40:37 +0200

    add MeasurementsScreen

---
+++ App/Containers/MeasurementsScreen.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { ScrollView, Text, KeyboardAvoidingView } from 'react-native'
+import { connect } from 'react-redux'
+// Add Actions - replace 'Your' with whatever your reducer is called :)
+// import YourActions from '../Redux/YourRedux'
+
+// Styles
+import styles from './Styles/MeasurementsScreenStyle'
+
+class MeasurementsScreen extends Component {
+  render () {
+    return (
+      <ScrollView style={styles.container}>
+        <KeyboardAvoidingView behavior='position'>
+          <Text>MeasurementsScreen</Text>
+        </KeyboardAvoidingView>
+      </ScrollView>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MeasurementsScreen)
--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,15 +1,16 @@
 import { StackNavigator } from 'react-navigation'
+import MeasurementsScreen from '../Containers/MeasurementsScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  MeasurementsScreen: { screen: MeasurementsScreen },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
-  headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
+  initialRouteName: 'MeasurementsScreen',
   navigationOptions: {
     headerStyle: styles.header
   }
